77555e28273e91a2fd8db8eeef5f07cd06694cfe
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-06-28 19:38:23 +0300

    Add files via upload

---
+++ Ödevler/ConsoleDeneme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleDeneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zaman = null;
+            DateTime saat24;
+            string sonuc = null;
+            
+            Console.WriteLine("12lik saati(AM/PM) 24 saat formatına çevirir");
+            Console.WriteLine("Saati 00:00:00AM formatında giriniz");
+            zaman = Console.ReadLine();
+
+            saat24 = DateTime.Parse(zaman);
+
+            sonuc = saat24.ToString("HH:mm:ss");
+
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
