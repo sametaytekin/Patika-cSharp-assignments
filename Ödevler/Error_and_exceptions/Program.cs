074055955cb0489938e199bbee96f603e76d3095
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-07-04 19:18:48 +0300

    Add files via upload

---
+++ Ödevler/Error_and_exceptions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Error_and_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi=0,sayi2=0;
+
+            Console.WriteLine("Bir sayı giriniz: ");
+            
+
+            try
+            {
+                sayi = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Yazı girilemez");
+                Console.WriteLine(ex);
+            }    
+
+            try
+            {
+                sayi2 = sayi / 0;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Sıfıra bölünemez!!!");
+                Console.WriteLine(ex);
+                
+            }
+
+
+            try
+            {
+                int sonuc = Int32.Parse(null);
+            }
+            catch(ArgumentNullException rx)
+            {   
+                Console.WriteLine("Null inte çevrilemez");
+                Console.WriteLine(rx);
+            }
+            finally
+            {
+                Console.WriteLine("İşlemler bitmiştir.");
+            }
+        }
+    }
+}
