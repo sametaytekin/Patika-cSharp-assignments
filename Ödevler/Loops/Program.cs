84f2920ee6c38c79da0aa05b0a970419995491ab
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-07-05 15:52:22 +0300

    Add files via upload

---
+++ Ödevler/Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For ile basamak yapma");
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi =Convert.ToInt32 (Console.ReadLine());
+
+
+            for(int i=0;i<sayi;i++){
+               
+
+                for(int k=0;k<=i;k++){
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+
+            for(int i=0;i<sayi;i++){
+                for(int k=sayi;k>i;k--){
+                   Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+
+            Console.WriteLine("");
+            /***************While örneği************/    
+            Console.WriteLine("While örneği");
+            int sayi2 = Convert.ToInt32( Console.ReadLine());
+            int sayac=1,toplam=0;
+
+            while (sayac <= sayi2)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Ortalama: "+toplam/sayi2);
+        }
+    }
+}
