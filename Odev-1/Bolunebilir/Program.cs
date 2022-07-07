89cb23f0e1c52a16a01da780962505c9cfa12ba3
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-07-07 15:47:53 +0300

    Add files via upload

---
+++ Odev-1/Bolunebilir/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Bolunebilir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miktar=0, bolen=0;
+            int[] sayilar;
+            Console.WriteLine("Miktar giriniz");
+            try
+            {
+                miktar = Convert.ToInt32( Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Yazı karakteri girmeyiniz");
+                Console.WriteLine(ex.Message.ToString());
+                throw;
+            }
+
+            if(miktar<0){
+                Console.WriteLine("Miktar negatif olamaz");
+            }
+
+            Console.WriteLine("Bolen giriniz");
+
+            try
+            {
+                bolen = Convert.ToInt32( Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("String değer girmeyiniz");
+                Console.WriteLine(ex.Message.ToString());
+                throw;
+            }
+
+            sayilar=new int[miktar];
+
+            Console.WriteLine("Girdiğiniz miktar kadar pozitif sayı giriniz");
+            for(int i=0;i<sayilar.Length;i++){
+                try
+                {
+                    sayilar[i] =Convert.ToInt32( Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("String değer girmeyiniz");
+                    Console.WriteLine(ex.Message.ToString());
+                    throw;
+                }
+            }
+            Console.WriteLine("Bölene eşit ve ya tam bölünebilen sayılar");
+
+            for(int j=0;j<sayilar.Length;j++){
+                if( sayilar[j]%bolen ==0 || sayilar[j] == bolen){
+                    Console.WriteLine(sayilar[j]);
+                }
+            }
+
+        }
+
+
+    }
+}
