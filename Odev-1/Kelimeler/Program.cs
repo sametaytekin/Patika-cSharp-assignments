ebf8f669f35ed7bbe2d5432fa5f93375cf868181
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-07-07 15:46:20 +0300

    Add files via upload

---
+++ Odev-1/Kelimeler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Kelimeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miktar=0;
+            string[] kelimeler;
+            Console.WriteLine("Pozitif Miktar giriniz");
+            
+            try
+            {
+                miktar =Convert.ToInt32( Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("String değer girmeyiniz");
+                Console.WriteLine(ex.Message.ToString());
+                throw;
+            }
+
+            kelimeler = new string[miktar];
+
+            Console.WriteLine("Miktar kadar kelime giriniz");
+
+            for(int i=0;i<kelimeler.Length;i++){
+                kelimeler[i] = Console.ReadLine().ToString();
+            }
+            Console.WriteLine("*****************");
+
+            Array.Reverse(kelimeler);
+
+            foreach (var kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+        }
+    }
+}
