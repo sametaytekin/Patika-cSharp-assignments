4b4c6b56a05d7747011e21efa2cdece54e28a6e3
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-07-07 15:46:49 +0300

    Add files via upload

---
+++ Odev-1/Cumle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Cumle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle = null;
+            int harfSayisi=0;
+            Console.WriteLine("Bir Cümle Giriniz!");
+
+            cumle = Console.ReadLine().ToString();
+
+            harfSayisi = kacHarfVar(cumle);
+
+            Console.WriteLine(harfSayisi);
+
+            int kelimeSayisi =cumle.kacKelimeVar();
+            Console.WriteLine(kelimeSayisi);
+        }
+
+        static int kacHarfVar(string metin){
+            short miktar=0;
+            foreach (char karakter in metin){
+                if(karakter == ' '){
+                    continue;
+                }
+                miktar++;
+            }
+            return miktar+1;
+        }
+
+    }
+
+    public static class MYExtension{
+        public static int kacKelimeVar (this string metin){
+            int miktar=0;
+            foreach (char karakter in metin)
+            {
+                if(karakter == ' '){
+                    miktar++;
+                }
+            }
+            return miktar;
+        }
+    }
+}
